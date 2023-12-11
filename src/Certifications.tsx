e08ab81e501492935a78945dc34a56d229ca72aa
HelOyBak <heleneoygarden@gmail.com>
2023-12-11 15:25:36 +0100

    first commit

---
+++ src/Certifications.tsx
@@ -0,0 +1,12 @@
+import { Card, CardHeader, Heading, CardBody, Stack, StackDivider, Box, Text } from "@chakra-ui/react";
+
+export const Certifications = () => (
+    <>
+        <Heading size='xs' textTransform='uppercase'>
+            Certifications
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+            Blah
+        </Text>
+    </>
+)
---
+++ src/Education.tsx
@@ -0,0 +1,12 @@
+import { Card, CardHeader, Heading, CardBody, Stack, StackDivider, Box, Text } from "@chakra-ui/react";
+
+export const Education = () => (
+    <>
+        <Heading size='xs' textTransform='uppercase'>
+            Education
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+            Blah
+        </Text>
+    </>
+)
