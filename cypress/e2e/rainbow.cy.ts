5c569b4c56d50d6be2cd47757a1225db941dbd5f
HelOyBak <heleneoygarden@gmail.com>
2024-02-06 12:44:56 +0100

    finished e2e test for rainbow effect

--- cypress/e2e/rainbow.cy.ts
+++ cypress/e2e/rainbow.cy.ts
@@ -1,8 +1,22 @@
+/// <reference types="Cypress" />
+
 describe('template spec', () => {
-  it('should change background to a rainbow party', () => {
+  it('should change the background with toggle switch', () => {
     cy.visit('/')
     cy.get('[data-cy="rainbow-switch"]').should('be.not.disabled');
     cy.get('[data-cy="rainbow-switch"]').click();
+    cy.get('body')
+      .should('not.have.css', 'background-color', 'rgb(251, 203, 116)')
     cy.get('[data-cy="rainbow-switch"]').click();
+    cy.get('body')
+      .should('have.css', 'background-color', 'rgb(251, 203, 116)')
+  });
+
+  it('should have a tooltip on toggle switch', () => {
+    cy.visit('/');
+    cy.contains('Do not flip the switch').should('not.exist');
+    cy.get('[data-cy="rainbow-switch"]').click().then(() => {
+      cy.contains('Do not flip the switch');
+    })
   })
 })
